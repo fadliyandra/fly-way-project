7603088f38e7e5a35df62c6911b48635b8096b3c
fadliyandra <fadliyandra27.fy@gmail.com>
2024-06-18 00:44:27 +0700

    first commit

---
+++ src/main/java/com/flyway/testflyway/model/User.java
@@ -0,0 +1,24 @@
+package com.flyway.testflyway.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "REGISTRATION_USERS")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String username;
+    private String first_name;
+    private String last_name;
+    private String enail;
+    private String mobile;
+
+    
+}
---
+++ src/test/java/com/flyway/testflyway/TestflywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyway.testflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestflywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
